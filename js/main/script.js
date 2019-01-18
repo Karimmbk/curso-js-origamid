23fc8b73b08ef6a452fc87f0392f8bfc96936328
Karimmbk <k171395@g.unicamp.br>
2019-01-18 21:03:51 -0200

    SAVEPOINT

--- js/main/script.js
+++ js/main/script.js
@@ -99,3 +99,46 @@
 // console.log(hr);
 
 // ---------------0306--------------------------
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+// const firstImg = document.querySelector('img').offsetTop;
+// console.log(firstImg);
+
+// // Retorne a soma da largura de todas as imagens
+// function somaImagens() {
+//   const imgs = document.querySelectorAll('img');
+//   let total = 0;
+//   imgs.forEach((img) => {
+//     total += img.offsetWidth;
+//     console.log(img.offsetWidth);
+//   });
+//   return total;
+// }
+
+// window.onload = () => {
+//   const total = somaImagens();
+//   console.log(`total = ${total}`);
+// };
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// // com o dedo. (48px/48px de acordo com o google)
+// const links = document.querySelectorAll('a');
+// links.forEach((link) => {
+//   if (link.getBoundingClientRect().width >= 48 && link.getBoundingClientRect().height >= 48) {
+//     console.log('ok');
+//   } else {
+//     console.log('n ok');
+//   }
+// });
+
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+// const tela = window.matchMedia('(max-width:720px)');
+// const menu = document.querySelector('.menu');
+
+// if (tela.matches) {
+//   menu.classList.add('menu-mobile');
+// }
+
+// ---------------0307--------------------------

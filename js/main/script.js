422584d0910c00e31a33723068fa3423c028a9cb
Karimmbk <k171395@g.unicamp.br>
2019-01-19 16:40:43 -0200

    SAVEPOINT

--- js/main/script.js
+++ js/main/script.js
@@ -281,3 +281,31 @@ function initAccordion() {
 initAccordion();
 
 // ---------------0311--------------------------
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollSuave(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const sectionTop = document.querySelector(href);
+
+    sectionTop.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    //   // forma alternativa e mais nova de se fazer
+    //   const topo = sectionTop.offsetTop;
+    //   window.scrollTo({
+    //     top: topo,
+    //     behavior: 'smooth',
+    //   });
+  }
+
+  linksInternos.forEach((element) => {
+    element.addEventListener('click', scrollSuave);
+  });
+}
+
+initScrollSuave();

71825a882169d7a7c4d36b12c4570321bc52b7e6
Karimmbk <k171395@g.unicamp.br>
2019-01-27 16:44:30 -0200

    modal add to the file

---
+++ js/main/modal.js
@@ -0,0 +1,37 @@
+// ---------------0503--------------------------
+function initModal() {
+  const botaoAbrir = document.querySelector('[data-modal="abrir"');
+  const botaoFechar = document.querySelector('[data-modal="fechar"');
+  const containerModal = document.querySelector('[data-modal="container"]');
+
+  // funcao para abrir o modal quando clicado no botao login
+  function abrirModal(event) {
+    event.preventDefault();
+    containerModal.classList.add('ativo');
+  }
+
+  // funcao para fechar o modal
+  function fecharModal(event) {
+    event.preventDefault();
+    containerModal.classList.remove('ativo');
+  }
+
+  // funcao para verificar onde esta sendo clicado para fechar o modal
+  function fecharContainerModal(event) {
+    event.preventDefault();
+    // eslint-disable-next-line max-len
+    // o 'event.target' vai pegar exatamente onde to clicando e o 'this' sempre vai ser a section q eh o objeto sendo         mandado para a funcao
+    if (event.target === this) {
+      fecharModal(event);
+    }
+  }
+
+  // verificando se as variaveis sao verdadeiras para chamar as funcoes
+  if (botaoAbrir && botaoFechar && containerModal) {
+    botaoAbrir.addEventListener('click', abrirModal);
+    botaoFechar.addEventListener('click', fecharModal);
+    containerModal.addEventListener('click', fecharContainerModal);
+  }
+}
+
+initModal();

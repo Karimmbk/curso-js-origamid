212a10d7420bc00235498ecb04c71665e827acca
Karimmbk <k171395@g.unicamp.br>
2019-01-27 17:13:40 -0200

    upgrade to only one function to open and close modal - toogleModal

--- js/main/modal.js
+++ js/main/modal.js
@@ -5,15 +5,9 @@ function initModal() {
   const containerModal = document.querySelector('[data-modal="container"]');
 
   // funcao para abrir o modal quando clicado no botao login
-  function abrirModal(event) {
+  function toogleModal(event) {
     event.preventDefault();
-    containerModal.classList.add('ativo');
-  }
-
-  // funcao para fechar o modal
-  function fecharModal(event) {
-    event.preventDefault();
-    containerModal.classList.remove('ativo');
+    containerModal.classList.toggle('ativo');
   }
 
   // funcao para verificar onde esta sendo clicado para fechar o modal
@@ -22,21 +16,22 @@ function initModal() {
     // eslint-disable-next-line max-len
     // o 'event.target' vai pegar exatamente onde to clicando e o 'this' sempre vai ser a section q eh o objeto sendo         mandado para a funcao
     if (event.target === this) {
-      fecharModal(event);
+      toogleModal(event);
     }
   }
+
   // funcao caso aperte a tecla esc tire o modal tb
   function escContainerModal(event) {
     const modalAtivo = containerModal.classList.contains('ativo');
     if (modalAtivo && event.key === 'Escape') {
-      fecharModal(event);
+      toogleModal(event);
     }
   }
 
   // verificando se as variaveis sao verdadeiras para chamar as funcoes
   if (botaoAbrir && botaoFechar && containerModal) {
-    botaoAbrir.addEventListener('click', abrirModal);
-    botaoFechar.addEventListener('click', fecharModal);
+    botaoAbrir.addEventListener('click', toogleModal);
+    botaoFechar.addEventListener('click', toogleModal);
     containerModal.addEventListener('click', fecharContainerModal);
     window.addEventListener('keyup', escContainerModal);
   }
